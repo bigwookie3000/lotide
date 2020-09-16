b1be6ab1391a4dad740274d74821e4eca4074044
Ryan Williams <ryanwilliams@Ryans-MacBook-Pro.local>
2020-09-15 22:27:19 -0400

    added new code

--- head.js
+++ head.js
@@ -1,12 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual.head([]) === expected) {
-    console.log(`✅✅✅ Assertion Passed:, ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  } 
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual.head([]) === expected) {
+//     console.log(`✅✅✅ Assertion Passed:, ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+//   } 
+// };
+const assertEqual = require('./assertEqual');
 
+const head = function (array) {
+  return array[0];
+}
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// // TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
