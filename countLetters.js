0ff6bb24b2c0701b0428d2514ea4f5986f20b2d7
Ryan Williams <bigwookie3000@gmail.com>
2020-09-16 20:54:27 -0400

    new working file

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:, ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  } 
+};
+
+const countLetters = function (numOfLetters) {
+  const results = {};
+
+  for (const numOf of numOfLetters) {
+    if (numOf !== ' ') {
+      if (results[numOf]) {
+      results[numOf] += 1;
+    } else {
+      results[numOf] = 1;
+    }
+  }
+}
+  return results;
+}
+
+console.log(countLetters("lighthouse in the house"));
+assertEqual(countLetters("lighthouse in the house")['s'], 2);
+assertEqual(countLetters("lighthouse in the house")['h'], 3);
